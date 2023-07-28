b6d54154b1b5fabf384c44e7aeedc2a667abab51
Mokoena13 <125538602+Mokoena13@users.noreply.github.com>
2023-07-28 15:44:21 +0200

    Update string.c

--- string.c
+++ string.c
@@ -1,29 +1,32 @@
 #include "shell.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- *
- * Return: integer length of string
+ * _strlen - Calculates the length of a string.
+ * @s: The input string whose length to check.
+ * Return: The length of the string as an integer.
  */
 int _strlen(char *s)
 {
-	int i = 0;
+	int j = 0;
 
 	if (!s)
 		return (0);
 
 	while (*s++)
-		i++;
-	return (i);
+		j++;
+	return (j);
 }
 
 /**
- * _strcmp - performs lexicogarphic comparison of two strangs.
- * @s1: the first strang
- * @s2: the second strang
+ * _strcmp - Performs lexicographic comparison of two strings.
+ * @s1: 1st string to compare.
+ * @s2: 2nd string to compare.
  *
- * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
+ * Return: An integer value less than, equal to, or greater than zero, depending on the
+ *		lexicographic order of the two strings.
+ *		- Negative value if s1 is lexicographically less than s2.
+ *		- Positive value if s1 is lexicographically greater than s2.
+ *		- Zero if s1 is lexicographically equal to s2.
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -41,11 +44,13 @@ int _strcmp(char *s1, char *s2)
 }
 
 /**
- * starts_with - checks if needle starts with haystack
- * @haystack: string to search
- * @needle: the substring to find
+ * starts_with - Checks if a string starts with a specific substring.
+ * @haystack: The string to search within.
+ * @needle: The substring to find at the beginning of the haystack.
  *
- * Return: address of next char of haystack or NULL
+ * Return: If the `needle` is found at the beginning of the `haystack`,
+ *		a pointer to the next character after the match is returned;
+ *		otherwise, NULL is returned.
  */
 char *starts_with(const char *haystack, const char *needle)
 {
@@ -56,11 +61,11 @@ char *starts_with(const char *haystack, const char *needle)
 }
 
 /**
- * _strcat - concatenates two strings
- * @dest: the destination buffer
- * @src: the source buffer
+ * _strcat - Concatenates two strings.
+ * @dest: The destination buffer where the source string will be appended.
+ * @src: The source buffer, containing the string to be appended.
  *
- * Return: pointer to destination buffer
+ * Return: A pointer to the destination buffer.
  */
 char *_strcat(char *dest, char *src)
 {
